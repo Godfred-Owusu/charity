4e8cb612aed5b672049dd348bf0eb5d3999276e8
Kobby7777 <godfredmirekuowusu@gmail.com>
2023-09-27 00:11:00 +0100

    Done setting up folders and tailwind configuration

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import About from "./Components/About/About";
+import Footer from "./Components/Footer/Footer";
+import Gallery from "./Components/Gallery/Gallery";
+import Hero from "./Components/Hero/Hero";
+import Navbar from "./Components/Navbar/Navbar";
+import Services from "./Components/Services/Services";
+import Testimonial from "./Components/Testimonial/Testimonial";
 
+// import Navbar from './Components/Navbar'
 function App() {
   return (
     <div>
-    
+    <Navbar />
+    <Hero />
+    <About />
+    <Services />
+    <Gallery />
+    <Testimonial />
+    <Footer />
     </div>
   );
 }
